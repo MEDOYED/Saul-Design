6f6b07b39359e3b8d8025c5d5b10e45eed7a7b43
MEDOYED <mmax26072005@gmail.com>
2024-04-08 21:07:50 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//  фильтр в секции works
+var mixer = mixitup('.target');
+
+// const sendButton = document.getElementById('sendButton');
+// const avatar = document.querySelector('.avatar');
+
+// function startAnimation() {
+//   avatar.classList.add('animate');
+// }
+
+
+// sendButton.addEventListener('click', startAnimation);
+
+// avatar.addEventListener('animationend', () => {
+//   avatar.classList.remove('animate');
+// });
