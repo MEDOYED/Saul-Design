2361e60cae4970ea2ea55327edcda3f00eb713fa
MEDOYED <mmax26072005@gmail.com>
2025-01-28 00:03:21 +0100

    corrected the css

--- src/components/widgets/work-item/work-item.jsx
+++ src/components/widgets/work-item/work-item.jsx
@@ -1,7 +1,3 @@
-import imgOne from "../../../assets/img/works/1.png";
-import imgTwo from "../../../assets/img/works/2.png";
-import imgThree from "../../../assets/img/works/3.png";
-
 import "./work-item.css";
 
 const WorkItem = ({ srcImg }) => {
